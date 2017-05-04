2a653781e705eeb18293af82f934237489ad3513
calvez <nemeth.lorant@clvz.hu>
2017-05-04 09:34:59 +0200

    initial

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+<div class="row">
+    <div class="col-xs-12 col-sm-4 col-sm-push-4">
+            <img src="https://i.imgur.com/sqGlVhU.png" class="thumbnail img-responsive">  footer credits 
+    </div>
+</div>
+</footer>
---
+++ vote.php
@@ -0,0 +1,62 @@
+<?php 
+include 'head.php';
+ ?>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8 col-sm-push-2" style="padding-top: 2em;">
+          <img src="images/hotellogo.png" class="text-center img-responsive">        
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-sm-8 col-sm-push-2 welcome-text">
+    <p class="text-justify">
+    <strong>Szavazzon</strong> a <a href="facebook.com/azevhotele" target="_blank">facebook.com/azevhotele</a> oldalon és
+    <strong>nyerjen</strong> egy wellness hétvégét az alábbi két hotel
+    egyikében, vagy további nyereményeink egyikét!</p>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 card">
+        <div class="thumbnail">
+            <img src="images/hotel_a.jpg" class="img-responsive" alt="#">
+          <div class="caption">
+            <h2 class="text-center hotelname">
+              MAXIMUM HOTEL<br/>
+              <small>
+                Pécs
+              </small>
+            </h2>
+            <hr>    
+            <p class="hotel-text">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            </p>
+            <button class="btn btn-default" role="button">Információk</button>
+                    <button class="btn btn-default" role="button">Rá szavazok</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 card">
+        <div class="thumbnail">
+            <img src="images/hotel_b.jpg" class="img-responsive" alt="#">
+          <div class="caption">
+            <h2 class="text-center hotelname">
+               Lorem ipsum dolor WELNESS SZÁLLÓ<br/>
+              <small>
+                Pécs
+              </small>
+            </h2>
+            <hr>    
+            <p class="hotel-text">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            </p>
+            <button class="btn btn-default" role="button">Információk</button>
+                    <button class="btn btn-default" role="button">Rá szavazok</button>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
+
+<?php 
+/* include 'footer.php';*/
+ ?>
